bac34b6c673ab111baac46e2434182e9b21cf6da
Alessandro Gilardoni <axelett@gmail.com>
2023-05-29 18:12:46 +0200

    Implement UIManager and update all text

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    private const byte MAX_LEVEL_DEATH = 1;
 
     [SerializeField] int _deaths;
     [SerializeField] bool _hasKey;
@@ -12,6 +15,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] Transform _player;
     [SerializeField] GemSpawner _gemSpawner;
     [SerializeField] int _gems;
+    [SerializeField] UIManager _uimanager;
 
     public bool HasKey { get { return _hasKey; } }
 
@@ -19,11 +23,13 @@ public class GameManager : MonoBehaviour
     {
         if(Instance != null)
         {
-            Destroy(this);
+            Destroy(gameObject);
+
         }
         else
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
     }
     // Start is called before the first frame update
@@ -40,6 +46,7 @@ public class GameManager : MonoBehaviour
     public void TakeKey()
     {
         _hasKey = true;
+        _uimanager.UpdateKey(true);
     }
 
     public void Restart()
@@ -53,10 +60,27 @@ public class GameManager : MonoBehaviour
     public void IncreaseDeath()
     {
         _deaths++;
+        _uimanager.UpdateDeaths(_deaths);
+        if (_deaths > MAX_LEVEL_DEATH)
+        {
+
+        }
     }
     public void IncreaseGem()
     {
         _gems++;
+        _uimanager.UpdateGems(_gems);
+    }
+
+    public void NextLevel()
+    {
+
+        Restart();
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex  + 1);
+        _uimanager.UpdateLevel(scene.buildIndex + 1);
+
+        
     }
 
 }
--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -11,4 +11,23 @@ public class UIManager : MonoBehaviour
     [SerializeField] TMP_Text _key_text;
     [SerializeField] TMP_Text _level_text;
 
+
+    public void UpdateLevel(int value)
+    {
+        _level_text.text = "Level " + value;
+    }
+    public void UpdateDeaths(int amount) {
+        _death_text.text = "Deaths: " + amount;
+    
+    }
+
+    public void UpdateKey(bool value)
+    {
+        _key_text.text = "Key " + value;
+    }
+
+    public void UpdateGems(int amount)
+    {
+        _gem_text.text =  "Gems: " + amount;
+    }
 }
